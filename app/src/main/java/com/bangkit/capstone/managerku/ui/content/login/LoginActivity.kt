abc6a78d1accc794596665ad13aaeff3144e0bca
ruligandari <ruligandari@gmail.com>
2021-05-30 10:29:31 +0700

    change font in login activity

--- app/src/main/java/com/bangkit/capstone/managerku/ui/content/login/LoginActivity.kt
+++ app/src/main/java/com/bangkit/capstone/managerku/ui/content/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.bangkit.capstone.managerku.ui.content.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.capstone.managerku.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
